1ca8b2450148c5b85fc5ac0f7063fbb2cd3e7ae3
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-10 12:11:33 +0100

    add: import express + starting config

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // start code
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
