4e0abf71898ce2bd3266f859b2938b11aac51ab8
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-10 12:23:04 +0100

    add: posts array

--- app.js
+++ app.js
@@ -5,8 +5,41 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Server del mio blog')
-})
+    res.send('Server del mio blog');
+
+    const posts = [
+        {
+            titolo: "Ricetta 1",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "public/imgs/ciambellone.jpeg",
+            tags: ["ricetta", "dolce", "ciambellone"]
+        },
+        {
+            titolo: "Ricetta 2",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "public/imgs/cracker_barbabietola.jpeg",
+            tags: ["ricetta", "salato", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 3",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "public/imgs/pane_fritto_dolce.jpeg",
+            tags: ["ricetta", "dolce", "pane"]
+        },
+        {
+            titolo: "Ricetta 4",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "public/imgs/pasta_barbabietola.jpeg",
+            tags: ["ricetta", "pasta", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 5",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "public/imgs/torta_paesana.jpeg",
+            tags: ["ricetta", "dolce", "torta"]
+        },
+    ];
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
