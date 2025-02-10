a74f54ac0667c9b71c41a88e344a51808b39410d
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-10 13:06:07 +0100

    fix: use public + res posts

--- app.js
+++ app.js
@@ -4,52 +4,55 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
+
     res.send('Server del mio blog');
 
-    // accesso a public
-
-    app.use(express.static('public'));
-
-    const posts = [
-        {
-            titolo: "Ricetta 1",
-            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
-            immagine: "public/imgs/ciambellone.jpeg",
-            tags: ["ricetta", "dolce", "ciambellone"]
-        },
-        {
-            titolo: "Ricetta 2",
-            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
-            immagine: "public/imgs/cracker_barbabietola.jpeg",
-            tags: ["ricetta", "salato", "barbabietola"]
-        },
-        {
-            titolo: "Ricetta 3",
-            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
-            immagine: "public/imgs/pane_fritto_dolce.jpeg",
-            tags: ["ricetta", "dolce", "pane"]
-        },
-        {
-            titolo: "Ricetta 4",
-            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
-            immagine: "public/imgs/pasta_barbabietola.jpeg",
-            tags: ["ricetta", "pasta", "barbabietola"]
-        },
-        {
-            titolo: "Ricetta 5",
-            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
-            immagine: "public/imgs/torta_paesana.jpeg",
-            tags: ["ricetta", "dolce", "torta"]
-        },
-    ];
 });
 
 // rotta bacheca
 
 app.get('/bacheca', (req, res) => {
 
-    res.send('Sono nella bacheca');
+    let posts = [
+
+        {
+            titolo: "Ricetta 1",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/ciambellone.jpeg",
+            tags: ["ricetta", "dolce", "ciambellone"]
+        },
+        {
+            titolo: "Ricetta 2",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/cracker_barbabietola.jpeg",
+            tags: ["ricetta", "salato", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 3",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/pane_fritto_dolce.jpeg",
+            tags: ["ricetta", "dolce", "pane"]
+        },
+        {
+            titolo: "Ricetta 4",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/pasta_barbabietola.jpeg",
+            tags: ["ricetta", "pasta", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 5",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/torta_paesana.jpeg",
+            tags: ["ricetta", "dolce", "torta"]
+        },
+    ];
+
+    // res -> risultato finale da mandare
+    res.json(posts);
+
 
 });
 
